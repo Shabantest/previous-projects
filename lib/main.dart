331791470d8d32ef01ce8a969f7200404ed476b9
Shaban <myApps@eraa.com>
2024-05-28 04:23:06 +0300

    the new images

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:graduation_project/view_model/data/local/shared_helper.dart';
-import 'package:graduation_project/view_model/data/local/shared_keyes.dart';
 import 'firebase_options.dart';
 import 'my_app.dart';
 
@@ -10,11 +10,29 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await SharedHelper.init();
-  var userToken =  await SharedHelper.get(key: SharedKeys.userToken);
-  print(userToken);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: true,
+    criticalAlert: true,
+    provisional: false,
+    sound: true,
+  );
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  print('User granted permission: ${settings.authorizationStatus}');
   runApp(EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ar')],
       path: 'assets/translations',
